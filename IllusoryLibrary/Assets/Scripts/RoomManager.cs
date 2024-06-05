5dfc0480184b8efffc4036ee5cec218061a53c19
Kreston101 <93973537+Kreston101@users.noreply.github.com>
2024-06-05 11:35:28 +0800

    Testing rooms

--- IllusoryLibrary/Assets/Scripts/RoomManager.cs
+++ IllusoryLibrary/Assets/Scripts/RoomManager.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
+    public GameObject[] exits;
+    public GameObject[] entrances;
     public GameObject player;
-    public GameObject[] levels;
-
-    private GameObject currentLevel;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentLevel = levels[0];
+        Instantiate(player);
     }
 
     // Update is called once per frame
